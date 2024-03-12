979401dd179a4542dabcfeeb713bbe3780893bca
Song Juhyun <juju020716@gmail.com>
2024-03-13 06:50:34 +0900

    set lifetime of each one's

--- src/grammar/mod.rs
+++ src/grammar/mod.rs
@@ -1,61 +1,66 @@
+mod options;
+mod test;
+
+use self::options::Option;
+
 use rand::prelude::*;
 use regex::Regex;
 use std::collections::HashMap;
-mod test;
 
-#[derive(Clone)]
-pub enum Any{
-    Usize(usize), Str(String), Tuple((Vec<String>, String))
-}
+
+// #[derive(Clone)]
+// pub enum Any{
+//     Usize(usize), Str(String), Tuple((Vec<String>, String))
+// }
 
 #[derive(Clone)]
 pub enum Union<A, B>{
     OnlyA(A), OnlyB(B)//, BOTH(A,B)
 }
-pub type Option  = HashMap<String, Any>;
-pub type Expansion = Union<String, (String, Option)>;
-pub type Grammar = HashMap<String, Vec<Expansion>>;
 
-pub fn nonterminals(expansion: &Expansion, re: &Regex) -> Vec<String> {
+pub type Expansion<'l_use> = Union<& 'l_use str, (& 'l_use str, Option<'l_use>)>;
+pub type Grammar<'l_use> = HashMap<& 'l_use str, Vec<Expansion<'l_use>>>;
+
+pub fn nonterminals<'l_use>(expansion: &Expansion<'l_use>, re: &Regex) -> Vec<& 'l_use str> {
    let expansion = match expansion{
         Union::OnlyA(only_str) =>{
-            only_str.clone()
+            only_str
         },
         Union::OnlyB(str_and_opt) =>{
-         str_and_opt.0.clone()
+         str_and_opt.0
         }
     };
 
     let ret = re
         .find_iter(&expansion)
-        .map(|m| String::from(m.as_str()))
+        .map(|m| m.as_str())
         .collect();
     return ret;
 }
-pub fn extend_grammar(grammar: &Grammar, extension: &Grammar)-> Grammar{
+pub fn extend_grammar<'l_use>(grammar: &Grammar<'l_use>, extension: Grammar<'l_use>)-> Grammar<'l_use>{
     let mut result = grammar.clone();
     for extension_tuple in extension{
-        match result.get_mut(extension_tuple.0){
+        match result.get_mut(&extension_tuple.0){
             Some(ref_result) =>{
-                *ref_result = extension_tuple.1.clone();
+                *ref_result = extension_tuple.1;
             },None =>{
-                result.insert(extension_tuple.0.clone(), extension_tuple.1.clone());
+                result.insert(extension_tuple.0, extension_tuple.1);
             }
         }
     }
     result
 }
 
-pub fn srange(sentence: & [char]) -> Vec<Expansion>{
+pub fn srange<'l_use>(sentence: & [char]) -> Vec<Expansion<'l_use>>{
     let mut result = Vec::with_capacity(sentence.len());
     for c in sentence{
         let k = format!("{}", c.clone());
-        result.push(Union::OnlyA(k))
+        result.push(Union::OnlyA(k.as_str()))
     }
     result
 }
 
-pub fn crange(start_char: char, end_char: char) -> Result<Vec<Expansion>, & 'static str>{
+pub fn crange<'l_use>(start_char: char, end_char: char) -> Result<Vec<Expansion<'l_use>>, & 'static str>{
     let diff = end_char as i32 - start_char as i32;
     if diff < 0 {
         return Err("character range is wrong!");
@@ -63,21 +68,21 @@ pub fn crange(start_char: char, end_char: char) -> Result<Vec<Expansion>, & 'sta
         let abs_diff = diff as usize;
         let mut result = Vec::with_capacity(abs_diff);
         for ascii_as_usize in start_char as u8..=end_char as u8{
-            let k = format!("{}", ascii_as_usize as char);
-            result.push(Union::OnlyA(k))
+            let k = format!("{}", ascii_as_usize.clone() as char);
+            result.push(Union::OnlyA(k.as_str()))
         }
         return Ok(result);
     }
 }
-pub fn new_symbol(grammar: &Grammar, symbol_name: &String) -> String{
+pub fn new_symbol<'l_use>(grammar: &Grammar, symbol_name: & 'l_use str) -> String{
     match grammar.get(symbol_name){
         Some(_) =>{
             let mut count = 1;
             loop{
-                let mut symbol_copy = symbol_name.clone();
+                let mut symbol_copy = String::from(symbol_name);
                 symbol_copy.pop();
                 let tentative_symbol_name = format!("{}-{}>", symbol_copy, count);
-                if !grammar.contains_key(&tentative_symbol_name){
+                if !grammar.contains_key(tentative_symbol_name.as_str()){
                     return tentative_symbol_name;
                 }
                 count += 1
@@ -88,18 +93,13 @@ pub fn new_symbol(grammar: &Grammar, symbol_name: &String) -> String{
 }
 
 
-pub fn opts(a: &[(String, Any)]) -> Option{
-    let mut result = HashMap::new();
-    for it in a{
-        result.insert(it.0.clone(), it.1.clone());
-    }
-    result
-}
-pub fn simple_grammar_fuzzer(
+
+
+pub fn simple_grammar_fuzzer<'l_use>(
     rd: &mut ThreadRng,
     re: &Regex,
     syntax: &Grammar,
-    start_symbol: &String,
+    start_symbol: & 'l_use str,
     max_nonterminals: usize,
     max_expansion_trials: usize,
     log: bool,
@@ -107,34 +107,35 @@ pub fn simple_grammar_fuzzer(
     let mut term = String::from(start_symbol);
     let mut expansion_trials = 0;
 
-    while nonterminals(&Union::OnlyA(term.clone()), re).len() > 0 {
-        let none_terminals = nonterminals(&Union::OnlyA(term.clone()), re);
+    while nonterminals(&Union::OnlyA(&term.clone()), re).len() > 0 {
+        let sub_nonterminals = term.clone();
+        let none_terminals = nonterminals(&Union::OnlyA(&sub_nonterminals), re);
         let rand_var = rd.gen_range(0..none_terminals.len());
-        let symbol_to_expand = none_terminals[rand_var].clone();
+        let symbol_to_expand = none_terminals[rand_var];
 
-        let expansions = &syntax[symbol_to_expand.as_str()];
+        let expansions = &syntax[symbol_to_expand];
 
         let rand_var2 = rd.gen_range(0..expansions.len());
         let expansion = &expansions[rand_var2];
 
         let expansion = match expansion{
             Union::OnlyA(a) =>{
-                a.clone()
+                a
             },
             Union::OnlyB(b) =>{
-                b.0.clone()
+                b.0
             }
         };
 
-        let new_term = term.replacen(&symbol_to_expand, &expansion, 1);
+        let new_term = (&term).replacen(&symbol_to_expand, &expansion, 1);
 
-        if nonterminals(&Union::OnlyA(new_term.clone()), re).len() < max_nonterminals {
+        if nonterminals(&Union::OnlyA(&new_term), re).len() < max_nonterminals {
             term = new_term;
             if log {
                 println!(
                     "{:<40} {}",
                     format!("{symbol_to_expand} -> {expansion}"),
-                    term
+                    &term
                 )
             }
             expansion_trials = 0;
---
+++ src/grammar/options.rs
@@ -0,0 +1,44 @@
+use std::collections::{HashMap};
+use crate::grammar::{Expansion, Union};
+use std::any::Any;
+
+pub type Option<'l_use>  = HashMap<&'l_use str, &'l_use dyn Any>;
+
+
+pub fn opts<'l_use>(a: &[(&'l_use str, &'l_use dyn Any)]) -> Option<'l_use>{
+    let mut result = HashMap::new();
+    for it in a{
+        result.insert(it.0, it.1);
+    }
+    result
+}
+
+pub fn exp_string<'l_use>(expansion: &Expansion<'l_use>) -> & 'l_use str{
+    match expansion{
+        Union::OnlyA(str_only) =>{
+            str_only.clone()
+        },
+        Union::OnlyB(str_and_map)=>{
+            str_and_map.0.clone()
+        }
+    }
+}
+
+pub fn exp_opts<'l_use>(expansion: &Expansion<'l_use>) -> HashMap<&'l_use str, &'l_use dyn Any>{
+    match expansion{
+        Union::OnlyA(_) =>{
+            HashMap::new()
+        },
+        Union::OnlyB(str_and_map)=>{
+            str_and_map.1.clone()
+        }
+    }
+}
+
+pub fn exp_opt<'l_use>(expansion: &Expansion<'l_use>, attribute: &'l_use str) -> std::option::Option<&'l_use dyn Any>{
+    match exp_opts(&expansion).get(attribute){
+        Some(ref_ref) =>{
+            Some(ref_ref.clone())
+        } None=> None
+    }
+}
