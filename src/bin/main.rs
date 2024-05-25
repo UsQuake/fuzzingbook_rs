5e6db196316bdb88a008cc492417b9773fb66daf
Song Juhyun <juju020716@gmail.com>
2024-05-25 14:04:00 +0900

    Intialize test harness for python

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,7 +3,7 @@ use fuzzingbook_rs::grammar::str_helper::*;
 use fuzzingbook_rs::grammar::*;
 use fuzzingbook_rs::grammar_fuzzer::var_ctx::*;
 use fuzzingbook_rs::grammar_fuzzer::*;
-use regex::Regex;
+use regex::Regex; 
 use std::time::{Duration, Instant};
 use std::{
     collections::*,
@@ -11,36 +11,31 @@ use std::{
 };
 fn main() {
     let mut f = GrammarsFuzzer::new(
-        &get_ruby_grammar(),
+        &get_python_grammar(),
         "<start>",
         10,
         100,
         Union::OnlyA(false),
     );
-    let count: u128 = 1;
-    let mut x_y_s = Vec::new();
+
     //let mut rand_seed = (SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis() & ((1<<65) - 1)) as u64;
     let mut rand_seed = 17526186317047798642;
-    let initial_seed_copy: u64 = rand_seed.clone();
-    for _ in 0..count {
-        let now = Instant::now();
-        let mutated_seed_copy: u64 = rand_seed.clone();
-        let testcase = f.fuzz(&mut rand_seed);
-        //let testcase = simple_grammar_fuzzer(&expr_grammar,"<start>", 15, 30, false).unwrap();
-        let elapsed = now.elapsed().as_millis();
-        unsafe{dbg!(CACHE_MISS_COUNT);
-        dbg!(CACHE_HIT_COUNT);}
-        x_y_s.push((testcase.len(), elapsed, testcase, mutated_seed_copy));
-    }
-    //17545 2.4s
+    let testcase = f.fuzz(&mut rand_seed);
+    std::fs::write("./testcase.py",  replace_scope_with_indent(&ir_to_ctx(&testcase, &mut rand_seed.clone()))).unwrap();
+    
+    let native_result = std::process::Command::new("python")
+        .arg("testcase.py")
+        .output()
+        .expect("failed to execute process");
+    let result_string =  String::from_utf8(native_result.stdout).unwrap();
+    println!("{}",result_string);
 
-    let mut i = 0;
-
-    for (_, elapsed, testcase, seed_copy) in &x_y_s {
-        i += elapsed;
-        std::fs::write("./a.rb",  replace_scope_with_indent(&ir_to_ctx(testcase, &mut seed_copy.clone()))).unwrap();
-    }
-
-    let avg = i / count;
-    println!("average elapsed time: {avg}ms");
+    let wasi_result = std::process::Command::new("./js")
+    .arg("run-wasi-py.js")
+    .arg("--")
+    .arg("none")
+    .output()
+    .expect("failed to execute process");
+    let result_string =  String::from_utf8(wasi_result.stdout).unwrap();
+    println!("{}",result_string);
 }
